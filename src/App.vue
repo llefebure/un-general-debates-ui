453b971f792279834b4563f84e44c8985ded8511
Luke Lefebure <lefebure@stanford.edu>
2019-04-09 21:08:06 -0400

    simple search app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,87 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ReactiveBase
+      app="debates"
+      url="https://u63n3n0euo:1q1xymals1@un-general-debate-7637131809.eu-central-1.bonsaisearch.net/"
+    >
+      <div class="navbar">
+        <h2>UN General Debate Speeches</h2>
+        <DataSearch
+          componentId="SearchString"
+          iconPosition="right"
+          :highlight="true"
+          :autosuggest="false"
+          :dataField="[
+            'content'
+          ]"
+          className="data-search"
+          :showClear="false"
+          queryFormat="and"
+          placeholder="Boolean string"
+      />
+      </div>
+      <button class="toggle" @click="switchContainer">
+        {{ showCards ? "Show Filters" : "Show Results" }}
+      </button>
+      <div class="container">
+        <div class="filters-container" :class="{ full: !showCards }">
+          <MultiList
+            componentId="CountryFilter"
+            dataField="country"
+            class="filter"
+            size=500
+            title="Select Countries"
+            selectAllLabel="All Countries"
+            :react="{ and: ['SearchString', 'YearFilter'] }"
+          />
+          <MultiList
+            componentId="YearFilter"
+            dataField="year"
+            class="filter"
+            size=100
+            title="Select Years"
+            selectAllLabel="All Years"
+            :react="{ and: ['SearchString', 'CountryFilter'] }"
+          />
+        </div>
+        <ReactiveList
+          componentId="SearchResult"
+          dataField="_id"
+          :pagination="true"
+          :class="{ full: showCards }"
+          :from="0"
+          :size="10"
+          :showResultStats="false"
+          className="result-list-container"
+          :react="{ and: ['SearchString', 'CountryFilter', 'YearFilter'] }"
+          :innerClass="{ list: 'result-item-container'}"
+        >
+          <div slot="renderData" slot-scope="{item}" class="mt-3">
+            <b-card class="text-left" :title="item.country" :sub-title="item.year">
+              <b-card-text class="text-left">
+                {{ item.content }}
+              </b-card-text>
+            </b-card>
+          </div>
+        </ReactiveList>
+      </div>
+    </ReactiveBase>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import "./styles.css";
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "app",
+  data: function() {
+    return {
+      showCards: window.innerWidth <= 768 ? true : false
+    };
+  },
+  methods: {
+    switchContainer: function() {
+      return (this.showCards = !this.showCards);
+    }
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ReactiveSearch from "@appbaseio/reactivesearch-vue";
+import BootstrapVue from 'bootstrap-vue'
+
+Vue.use(ReactiveSearch);
+Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 
 new Vue({
   render: h => h(App),
